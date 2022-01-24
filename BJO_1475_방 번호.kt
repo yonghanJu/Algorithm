134702cca87a4a7adbc02551360aa6eeb5d1a098
yonghanJu <65655825+yonghanJu@users.noreply.github.com>
2022-01-24 22:21:02 +0900

    Create BJO_1475_방 번호.kt

---
+++ BJO_1475_방 번호.kt
@@ -0,0 +1,17 @@
+// 2022-01-24
+// https://www.acmicpc.net/submit/1475/38050305
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    var arr = IntArray(10)
+    var answer =0
+    val n = readLine().forEach{arr[it.toString().toInt()]++}
+    arr.forEachIndexed{index, it->if((index!=6 && index !=9) && answer<it) answer =it}
+    val a2 = if((arr[6]+arr[9])%2==0) (arr[6]+arr[9])/2 else (arr[6]+arr[9])/2+1
+    print(maxOf(answer, a2))
+}
