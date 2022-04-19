2457f5a04c98383731bbc7369e4525487ec709c6
yonghanJu <65655825+yonghanJu@users.noreply.github.com>
2022-04-20 00:34:03 +0900

    Update Math_BOJ_1010_다리놓기.kt

--- Math_BOJ_1010_다리놓기.kt
+++ Math_BOJ_1010_다리놓기.kt
@@ -6,15 +6,26 @@ import java.util.*
 import kotlin.math.*
 
 class Solution{
+
+    lateinit var list:Array<IntArray>
+
     fun solution(n:Int, r:Int):Int{
-        var cur = BigInteger("1")
-        for(i in 0 until  r){
-            cur *= BigInteger((n-i).toString())
-        }
-        for(i in 1 .. r){
-            cur /= BigInteger(i.toString())
-        }
-        return cur.toInt()
+        var answer =0
+        list = Array(n+1){IntArray(r+1){-1}}
+        return re(n,r)
+    }
+
+    // 4C3 = 3C2 + 3C3
+    fun re(n:Int, r:Int):Int{
+        if(n==r || r==0)  return 1
+        if(list[n][r]>0) return list[n][r]
+        var a = list[n-1][r-1]
+        var b = list[n-1][r]
+        if(a==-1) a = re(n-1,r-1)
+        if(b==-1) b =re(n-1,r)
+        list[n-1][r-1] = a
+        list[n-1][r] = b
+        return a+b
     }
 }
 
