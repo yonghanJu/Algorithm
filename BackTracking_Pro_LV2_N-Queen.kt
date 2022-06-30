f105de7d3abb272d00856302659d2ed8a46ffde6
yonghanJu <ju990828@naver.com>
2022-07-01 01:16:53 +0900

    Update BackTracking_Pro_LV2_N-Queen.kt

--- BackTracking_Pro_LV2_N-Queen.kt
+++ BackTracking_Pro_LV2_N-Queen.kt
@@ -8,32 +8,29 @@ class Solution {
     
     fun solution(n: Int): Int {
         board = Array(4){BooleanArray(n*2)}
-        dfs(0,0,0,n)
+        dfs(0,0,n)
         return answer
     }
     
-    fun dfs(x:Int, y:Int, count:Int, size:Int){
+    fun dfs(x:Int, count:Int, size:Int){
         if(count==size){
             answer++
             return
         }
-        if(y==size){
-            dfs(x+1,0,count,size)
-            return
-        }
-        if(x==size) return
+        if(x>=size) return
         
-        dfs(x,y+1,count,size)
-        if(board[0][x].not() && board[1][y].not() && board[2][x+y].not() && board[3][x-y+size].not()){
-            board[0][x] = true
-            board[1][y] = true
-            board[2][x+y] = true
-            board[3][x-y+size] = true
-            dfs(x,y+1,count+1,size)
-            board[0][x] = false
-            board[1][y] = false
-            board[2][x+y] = false
-            board[3][x-y+size] = false
+        for(y in 0 until size){
+            if(board[0][x].not() && board[1][y].not() && board[2][x+y].not() && board[3][x-y+size].not()){
+                board[0][x] = true
+                board[1][y] = true
+                board[2][x+y] = true
+                board[3][x-y+size] = true
+                dfs(x+1,count+1,size)
+                board[0][x] = false
+                board[1][y] = false
+                board[2][x+y] = false
+                board[3][x-y+size] = false
+            }
         }
     }
 }
