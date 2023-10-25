41d73da5c549e2800769c125bf935e70deaf01c6
JU YONGHAN <ju990828@naver.com>
2023-10-25 10:11:28 +0900

    Update Greedy_BOJ_깊콘이 흘러넘쳐.kt

--- Greedy_BOJ_깊콘이 흘러넘쳐.kt
+++ Greedy_BOJ_깊콘이 흘러넘쳐.kt
@@ -12,23 +12,20 @@ fun main() {
     val pq = PriorityQueue(compareBy<Triple<Int, Int, Int>> { it.third }.thenBy { it.second })
     for (idx in gifts.indices) {
         val it = gifts[idx]
-        var ex = 0
-        if (plans[idx] > gifts[idx]) {
-            ex = (plans[idx] - gifts[idx]) / 30 + if((plans[idx] - gifts[idx]) % 30 == 0) 0 else 1
-            answer += ex.toLong()
-        }
+        val ex = (plans[idx] - gifts[idx]) / 30 + if ((plans[idx] - gifts[idx]) % 30 == 0) 0 else 1
+        answer += ex.toLong()
         gifts[idx] = it + 30 * ex
         pq.add(Triple(idx, gifts[idx], plans[idx]))
     }
     var max = 0
     while (pq.isNotEmpty()) {
         var p = pq.poll()
-        if (max > p.second) {
-            val ex = (max - p.second) / 30 + if((max - p.second) % 30 == 0) 0 else 1
+        if (max > p.second) { // 사용 못하는 경우
+            val ex = (max - p.second) / 30 + if ((max - p.second) % 30 == 0) 0 else 1
             answer += ex.toLong()
             p = p.copy(second = p.second + 30 * ex)
             pq.add(p)
-        } else {
+        } else { // 바로 사용 가능
             max = p.second
         }
     }
