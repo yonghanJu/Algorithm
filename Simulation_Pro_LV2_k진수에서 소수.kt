6146fe1ec01ae3b18d7bd523528b28613d42ccbb
JU YONGHAN <ju990828@naver.com>
2023-11-08 00:39:40 +0900

    Update Simulation_Pro_LV2_k진수에서 소수.kt

--- Simulation_Pro_LV2_k진수에서 소수.kt
+++ Simulation_Pro_LV2_k진수에서 소수.kt
@@ -1,62 +1,29 @@
-// 2022-06-30
-// https://programmers.co.kr/learn/courses/30/lessons/92335# (k진수에서 소수 개수 구하기)
+import kotlin.math.*
 
 class Solution {
     fun solution(n: Int, k: Int): Int {
         var answer: Int = 0
-        var str = StringBuilder()
-        var num = n
         
-        while(num!=0){
-            // StringBuilder 문단 앞에 삽입
-            str = str.insert(0,(num%k).toString())
-            num/=k
+        var kNum = ""
+        var cur = n
+        while(cur > 0) {
+            kNum = "${cur%k}$kNum"
+            cur /= k
         }
         
-        // 정규식 활용 split
-        val list = String(str).split('0')
-        
-        //val max = list.maxOrNull() ?: return 0
-        //val isPrime = BooleanArray(max+1){true}
-        
-        // 1. 배열을 돌면서 소수 여부를 판단하기 때문에 사전 정의를 잘 하자
-        // 1을 소수가 아닌걸로 미리 정의
-        // 2. 인덱스에 직접 접근할 떄는 항상 조심
-        
-        // 소수판별 에라토스테 체 알고리즘
-        // 이번 알고리즘에선 int 범위를 넘어가므로 사용 x
-//         if(max != 0) isPrime[1]=false
-        
-//         var i = 2
-//         while(i<=max){
-//             if(isPrime[i].not()) {
-//                 i++
-//                 continue
-//             }
-//             var j = i*i
-//             while(j<=max){
-//                 isPrime[j]=false
-//                 j += i
-//             }
-//             i++
-//         }
-        
-        for(it in list){
-            if(it=="") continue
-            if(isPrime(it.toLong())) answer++
+        kNum.split("0").filter { it.length > 0 }.forEach {
+            answer += isPrime(it.toLong())
         }
         return answer
     }
     
-    fun isPrime(n:Long):Boolean{
-        // 1일 때 예외 처리
-        // 항상 범위를 설정할 땐 예외를 처리
-        if(n<2L) return false
-        
-        // 소수판별 범위 설정 시 Math.sqrt 사용
-        for(i in 2 .. Math.sqrt(n.toDouble()).toLong()){
-            if(n%i==0L) return false
+    private fun isPrime(n: Long): Int {
+        if(n == 1L) return 0
+        var i = 2L
+        while(i <= sqrt(n.toDouble())) {
+            if(n % i == 0L) return 0
+            i++
         }
-        return true
+        return 1
     }
 }
