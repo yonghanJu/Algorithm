ddb62d5b52312746af87efa9502ceeb7c01291f0
JU YONGHAN <ju990828@naver.com>
2023-11-08 09:40:13 +0900

    Update Hashing_Pro_LEVEL2_오픈채팅방.kt

--- Hashing_Pro_LEVEL2_오픈채팅방.kt
+++ Hashing_Pro_LEVEL2_오픈채팅방.kt
@@ -1,37 +1,19 @@
 class Solution {
     fun solution(record: Array<String>): Array<String> {
-        var answer = mutableListOf<String>()
-        val map = HashMap<String,String>()
-        val isLeaved = HashMap<String,Boolean>()
-        record.forEach{
-            val split = it.split(' ')
-            when(split[0]){
-                "Enter"->{
-                    map[split[1]]=split[2]
-                    isLeaved[split[1]] = false
-                }
-                "Leave"->{
-                    isLeaved[split[1]] = true
-                }
-                "Change"->{
-                    if(isLeaved[split[1]] == false){
-                        map[split[1]] = split[2]
-                    }
-                }
+        val nickMap = mutableMapOf<String, String>()
+        var answer = mutableListOf<Pair<String, String>>()
+        record.forEach {
+            val history = it.split(' ')
+            if(history[0] == "Change" || history[0] == "Enter") {
+                nickMap[history[1]] = history[2]!!
+            }
+            if(history[0] == "Enter") {
+                answer.add(Pair(history[1], "Enter"))
+            } else if(history[0] == "Leave") {
+                answer.add(Pair(history[1], "Leave"))
             }
         }
         
-        record.forEach{
-            val split = it.split(' ')
-            when(split[0]){
-                "Enter"->{
-                    answer.add("${map[split[1]]}님이 들어왔습니다.")
-                }
-                "Leave"->{
-                    answer.add("${map[split[1]]}님이 나갔습니다.")
-                }
-            }
-        }
-        return Array(answer.size){answer[it]}
+        return answer.map { (uid, action) -> "${nickMap[uid]}님이 ${if(action == "Enter") "들어왔습니다" else "나갔습니다"}." }.toTypedArray()
     }
 }
