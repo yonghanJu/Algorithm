756389a61e654d1a8cb8636569c46cad1903826c
JU YONGHAN <ju990828@naver.com>
2023-11-24 13:51:09 +0900

    Create BackTracking_Pro_외벽 점검.kt

---
+++ BackTracking_Pro_외벽 점검.kt
@@ -0,0 +1,46 @@
+
+class Solution {
+    var answer = Int.MAX_VALUE
+    val answerMap = mutableMapOf<Set<Int>, Int>()
+    fun solution(n: Int, weak: IntArray, dist: IntArray): Int {
+        dfs(0, 0, weak.toSet(), dist.sortedBy { -it }, n)
+
+        return if (answer == Int.MAX_VALUE) -1 else answer
+    }
+
+    fun dfs(distIndex: Int, distCount: Int, remainWeaks: Set<Int>, dist: List<Int>, n: Int) {
+        if ((answerMap[remainWeaks] ?: Int.MAX_VALUE) <= distCount) return
+        answerMap[remainWeaks] = distCount
+
+        if (remainWeaks.isEmpty()) {
+            answer = minOf(distIndex, answer)
+            return
+        }
+        if (distIndex == dist.size) {
+            return
+        }
+
+        remainWeaks.forEach { startIndex ->
+            val endIndex = startIndex + dist[distIndex]
+
+            if (endIndex >= n) {
+                dfs(
+                    distIndex + 1,
+                    distCount + 1,
+                    remainWeaks.filter { (it in (startIndex until n) || it in (0..endIndex - n)).not() }.toSet(),
+                    dist,
+                    n,
+                )
+            } else {
+                dfs(
+                    distIndex + 1,
+                    distCount + 1,
+                    remainWeaks.filter { (it in (startIndex..endIndex)).not() }.toSet(),
+                    dist,
+                    n,
+                )
+            }
+        }
+        dfs(distIndex + 1, distCount, remainWeaks, dist, n)
+    }
+}
