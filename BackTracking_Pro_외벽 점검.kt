281059a20a740275e4d770765af596aa0458f2d5
JU YONGHAN <ju990828@naver.com>
2023-11-24 14:00:07 +0900

    Update BackTracking_Pro_외벽 점검.kt

--- BackTracking_Pro_외벽 점검.kt
+++ BackTracking_Pro_외벽 점검.kt
@@ -1,17 +1,13 @@
 class Solution {
     var answer = Int.MAX_VALUE
-    val answerMap = mutableMapOf<Set<Int>, Int>()
 
     fun solution(n: Int, weak: IntArray, dist: IntArray): Int {
-        dfs(0, 0, weak.toSet(), dist.sortedBy { -it }, n)
+        dfs(0, weak.toSet(), dist.sortedBy { -it }, n)
 
         return if (answer == Int.MAX_VALUE) -1 else answer
     }
 
-    fun dfs(distIndex: Int, distCount: Int, remainWeaks: Set<Int>, dist: List<Int>, n: Int) {
-        if ((answerMap[remainWeaks] ?: Int.MAX_VALUE) <= distCount) return
-        answerMap[remainWeaks] = distCount
-
+    fun dfs(distIndex: Int, remainWeaks: Set<Int>, dist: List<Int>, n: Int) {
         if (remainWeaks.isEmpty()) {
             answer = minOf(distIndex, answer)
             return
@@ -24,12 +20,12 @@ class Solution {
             val endIndex = startIndex + dist[distIndex]
 
             val remain = if (endIndex >= n) {
-                remainWeaks.filter { (it in (startIndex until n) || it in (0..endIndex - n)).not() }.toSet(),
+                remainWeaks.filter { (it in (startIndex until n) || it in (0..endIndex - n)).not() }.toSet()
             } else {
                 remainWeaks.filter { (it in (startIndex..endIndex)).not() }.toSet()
             }
-            dfs(distIndex + 1, distCount + 1, remain, dist, n)
+            dfs(distIndex + 1, remain, dist, n)
         }
-        dfs(distIndex + 1, distCount, remainWeaks, dist, n)
+        dfs(distIndex + 1, remainWeaks, dist, n)
     }
 }
